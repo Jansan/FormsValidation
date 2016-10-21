7507c000148c2533d464b025ef1e10337746a48d
jansan mariyathas <jansan-26@hotmail.com>
2016-10-21 10:40:39 +0200

    all done

---
+++ Scripts/Components/ValidatedInput/validated-input.component.js
@@ -0,0 +1,19 @@
+﻿angular.module("mainModule")
+    .component("validatedInput", {
+        templateUrl: "Scripts/Components/ValidatedInput/ValidatedInput.html",
+        controller: function () {
+            var ctrl = this;
+
+            ctrl.setValid = function (isValid) {
+                console.log(isValid);
+            }
+        },
+        binding: {
+            type: "<",
+            id: "<",
+            lable: "<",
+            model: "=",
+            isValid: "="
+
+        }
+    });
